a8b42d098e197b4e7b72b03a1407235272b27d4d
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-14 21:41:53 +0100

    test: First embedded test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().into()
 }
 
 #[tokio::main]
@@ -10,4 +10,15 @@ async fn main() -> Result<(), std::io::Error> {
         .bind("127.0.0.1:8000")?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
 }
