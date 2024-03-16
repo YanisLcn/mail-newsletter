b5ffb14b1d789d835602ab3b78013ed8d1cb8dcf
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-16 13:36:59 +0100

    feat: Add configuration for app launch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
 
-use mail_newsletter::run;
+use mail_newsletter::configuration::get_configuration;
+use mail_newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
