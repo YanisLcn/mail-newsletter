ea6f941e8f81ee53aabf54f78547464b87cf3ab4
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-03-20 23:39:40 +0100

    feat: Logs `GET` requests

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::net::TcpListener;
 use mail_newsletter::configuration::get_configuration;
 use mail_newsletter::startup::run;
 use sqlx::PgPool;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
