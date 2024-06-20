ec131bae387ddb7338cf0c67ad19dbe71225d74a
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-06-20 11:00:41 +0200

    feat: Use Instrument tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,16 +14,9 @@ pub async fn suscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Htt
     let request_id = Uuid::new_v4();
     let request_span = tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name = %form.name);
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "[{}] - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "[{}] - Saving new subscriber details in database",
-        request_id
-    );
+
+    let query_span = tracing::info_span!("Saving new subscriber details in database");
+
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         request_id,
@@ -31,6 +25,7 @@ pub async fn suscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Htt
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
@@ -38,7 +33,7 @@ pub async fn suscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Htt
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            println!("[{request_id}] - Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
